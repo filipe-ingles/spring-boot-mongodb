b4e3bc0660ef75e1c6dd0946bdb7e9b7311bacd9
filipe-ingles <filipeafonsoingles@gmail.com>
2023-01-22 12:47:38 -0300

    User insertion with POST

--- src/main/java/com/filipeafonso/springmongo/services/UserService.java
+++ src/main/java/com/filipeafonso/springmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.filipeafonso.springmongo.domain.User;
+import com.filipeafonso.springmongo.dto.UserDTO;
 import com.filipeafonso.springmongo.repositories.UserRepository;
 import com.filipeafonso.springmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert (User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 }
