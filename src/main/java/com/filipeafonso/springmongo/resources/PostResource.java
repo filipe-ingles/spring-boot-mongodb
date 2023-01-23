dc27575ed0c5e45ab381cff33a78ce8eac782cf3
filipe-ingles <filipeafonsoingles@gmail.com>
2023-01-22 23:56:42 -0300

    Search with query methods

--- src/main/java/com/filipeafonso/springmongo/resources/PostResource.java
+++ src/main/java/com/filipeafonso/springmongo/resources/PostResource.java
@@ -1,26 +1,18 @@
 package com.filipeafonso.springmongo.resources;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.filipeafonso.springmongo.domain.Post;
-import com.filipeafonso.springmongo.domain.User;
-import com.filipeafonso.springmongo.dto.UserDTO;
+import com.filipeafonso.springmongo.resources.util.URL;
 import com.filipeafonso.springmongo.services.PostService;
-import com.filipeafonso.springmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -33,5 +25,12 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = postService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
