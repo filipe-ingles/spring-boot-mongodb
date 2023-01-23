95b2339e3ed5f39ccd22a299120e712b3db9754e
filipe-ingles <filipeafonsoingles@gmail.com>
2023-01-22 23:25:41 -0300

    User posts endpoint created

--- src/main/java/com/filipeafonso/springmongo/resources/UserResource.java
+++ src/main/java/com/filipeafonso/springmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.filipeafonso.springmongo.domain.Post;
 import com.filipeafonso.springmongo.domain.User;
 import com.filipeafonso.springmongo.dto.UserDTO;
 import com.filipeafonso.springmongo.services.UserService;
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = userService.update(obj);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
