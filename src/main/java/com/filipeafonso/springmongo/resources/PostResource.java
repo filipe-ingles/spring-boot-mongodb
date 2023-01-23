4639d72a0d1f264f62c8a37f1bf08b931ecb8bf0
filipe-ingles <filipeafonsoingles@gmail.com>
2023-01-22 23:31:32 -0300

    Find Posts by Id

---
+++ src/main/java/com/filipeafonso/springmongo/resources/PostResource.java
@@ -0,0 +1,37 @@
+package com.filipeafonso.springmongo.resources;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.filipeafonso.springmongo.domain.Post;
+import com.filipeafonso.springmongo.domain.User;
+import com.filipeafonso.springmongo.dto.UserDTO;
+import com.filipeafonso.springmongo.services.PostService;
+import com.filipeafonso.springmongo.services.UserService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService postService;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = postService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
