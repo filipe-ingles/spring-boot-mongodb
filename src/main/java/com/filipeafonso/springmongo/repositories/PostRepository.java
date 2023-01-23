f2c06cdc77e46468aac68cf3ca2e4a66549e01c1
filipe-ingles <filipeafonsoingles@gmail.com>
2023-01-23 00:02:56 -0300

    Search with @Query

--- src/main/java/com/filipeafonso/springmongo/repositories/PostRepository.java
+++ src/main/java/com/filipeafonso/springmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.filipeafonso.springmongo.domain.Post;
@@ -11,5 +12,8 @@ import com.filipeafonso.springmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 
 }
